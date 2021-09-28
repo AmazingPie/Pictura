3372f9fab23c571a23a12985e98875be51f0e5f4
AmazingPie <cjrulesgotit@gmail.com>
2021-09-28 21:12:14 +0100

    Replace custom thumbnail resizing with PIL function
    scale_to_canvas() was originally made as a solution to scale the size of an
    image so that it would fit inside a given area/canvas while maintaining the
    aspect ratio. While this worked, to reduce the code complexity we can replace
    it with a function in PIL that was designed for that purpose:
    Image.thumbnail().

--- src/file/image_file.py
+++ src/file/image_file.py
@@ -17,33 +17,9 @@ class ImageFile(File):
         super().create_window()
         self.image_window = iw.ImageWindow(self.window, self.filename)
 
-    """ Scale an image to the size of the canvas that contains it.
-
-    Make the image the maximum size possible so that either the width or height
-    is the same as the canvas width or height respectively. Also maintain the
-    aspect ratio of the image when rescaling.
-
-    :param img_width:   width of the image to scale
-    :param img_height:  height of the image to scale
-    :param can_width:
-    :returns:           a scaled (width, height) size tuple
-    """
-    def scale_to_canvas(self, img_width, img_height, can_width, can_height):
-        new_width = 0
-        new_height = 0
-        if (img_width < img_height):
-            new_height = can_height
-            new_width = int(img_width / (img_height / can_height))
-        else:
-            new_width = can_width
-            new_height = int(img_height / (img_width / can_width))
-
-        return (new_width, new_height)
-
     """ Return a thumbnail of given height and width for this image. """
     def get_thumbnail(self, width, height):
         # Make the scaled image
         img = Image.open(self.filename)
-        (width, height) = self.scale_to_canvas(*img.size, width, height)
-        img = img.resize((width, height), Image.LANCZOS)
+        img.thumbnail((width, height))
         return ImageTk.PhotoImage(img)
